05f6d186f6e319222f1ffbebf9846efd68886978
Гарик Нурмуродоff <garikn95@gmail.com>
2018-10-29 18:31:17 +0500

    Port changed to 3000 for Prod upload

--- app.js
+++ app.js
@@ -63,6 +63,6 @@ io.on('connection', function(socket){
 
 });
 
-server.listen(8443, function () {
-    console.log('Example app listening on port 8443!');
+server.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
 });
